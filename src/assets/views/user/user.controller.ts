068aa34b9f73af7e5df5c163907c6176b8010d0d
karadenizemirr <karadenizemirr@gmail.com>
2023-09-15 16:59:09 +0300

    template Update

--- src/assets/views/user/user.controller.ts
+++ src/assets/views/user/user.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 
 @Controller()
 export class UserController {
     constructor() {}
+
+    @Get()
+    async login(){
+        return {
+            title: 'Login Page'
+        }
+    }
 }
